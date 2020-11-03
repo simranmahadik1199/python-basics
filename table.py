e397c0169025dd134c8e02ca52db47d7505c694c
simran <simranmahadik99@gmail.com>
2020-11-03 21:14:18 +0530

    table of number

--- table.py
+++ table.py
@@ -1,4 +1,4 @@
-
+#type1
 def mult():
     
     n=int(input('enter an number'))
@@ -7,20 +7,21 @@ def mult():
         table=num*n
         print(table)
         
+        
         tbl=('table  of {} is {} , {}'.format(n,num,n*num))
         num=num+1
     return table
 table=mult()
 print(table)
-mult()   
+mult() 
 
-
+#type2
 def tbl():
     number=int(input('enter a number'))
     for x in range(1,11,1):
         table=('table is {} multiplied by {} equal to {}'.format(number,x,number*x))
         print(table)
-tbl()       
+tbl()     
 
 
 
