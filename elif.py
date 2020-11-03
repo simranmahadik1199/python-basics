9364781f9324ad6ade0cfdeb404838b731c172af
simran <simranmahadik99@gmail.com>
2020-11-03 22:08:24 +0530

    else-if

---
+++ elif.py
@@ -0,0 +1,26 @@
+number=float(input('enter a number'))
+if number==0:
+    print('zero')
+elif number==1:
+    print('one')
+elif number==2:
+     print('two')
+elif number==3:
+     print('three')
+elif number==4:
+     print('four')
+elif number==5:
+     print('five')
+elif number==6:
+     print('six')
+elif number==7:
+     print('seven')
+elif number==8:
+     print('eight')
+elif number==9:
+     print('nine')
+else:
+    print('enter between 0 to 9')
+    
+
+     
