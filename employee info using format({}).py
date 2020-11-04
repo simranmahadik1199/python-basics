dc46235c2552081288261b8e2270bc8c7bf22493
simran <simranmahadik99@gmail.com>
2020-11-04 21:22:37 +0530

    using formating({})

---
+++ employee info using format({}).py
@@ -0,0 +1,4 @@
+Ename=input('Enter Employee Name')
+EID=int(input('Enter Employee ID'))
+ESal=float(input('Enter Employee Salary'))
+print('Employee Name={} Employee ID={} Employee Salary={}'.format(Ename,EID,ESal))
