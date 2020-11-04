6a8ede123954209c085508bb67ae94b8aa2f52f6
simran <simranmahadik99@gmail.com>
2020-11-04 21:26:56 +0530

    multiplication table

---
+++ table.py
@@ -0,0 +1,27 @@
+#type1
+def mult():
+    
+    n=int(input('enter an number'))
+    num=0
+    while num<=10:
+        table=num*n
+        print(table)
+        
+        
+        tbl=('table  of {} is {} , {}'.format(n,num,n*num))
+        num=num+1
+    return table
+table=mult()
+print(table)
+mult() 
+
+#type2
+def tbl():
+    number=int(input('enter a number'))
+    for x in range(1,11,1):
+        table=('table is {} multiplied by {} equal to {}'.format(number,x,number*x))
+        print(table)
+tbl()     
+
+
+
