cbbb6cd2f20b26f3588bcaff2cc1bd6b5ffe0523
simran <simranmahadik99@gmail.com>
2020-11-04 22:07:38 +0530

    using formating (modifier)

---
+++ emp info using  format (modifier).py
@@ -0,0 +1,7 @@
+Ename=input('Enter Employee Name')
+EID=int(input('Enter Employee ID'))
+ESal=float(input('Enter Employee Salary'))
+print('Employee Name:%s'%Ename)
+print('Employee ID:%i'%EID)
+print('Employee Salary:%f'%ESal)
+
